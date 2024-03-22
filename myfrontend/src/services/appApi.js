6212bdaae3db74a69db2bf9ec039392809d01aa7
laila22haz <hazmirlaila316@gmail.com>
2024-03-22 00:37:35 +0000

    let's see if it will work for u

--- myfrontend/src/services/appApi.js
+++ myfrontend/src/services/appApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const appApi = createApi({
     reducerPath: 'appApi',
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }), // Corrected property name
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
     endpoints: (builder) => ({
         signup: builder.mutation({
             query: (user) => ({
