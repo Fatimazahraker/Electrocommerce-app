d16af605fbed6b8f7b8fe8c2a4a693eae65a0c28
laila22haz <hazmirlaila316@gmail.com>
2024-03-26 00:38:01 +0000

    Add the table where the product amd the price and quantity exist + increase & decrease the quatity without missing styling it with css and ability to remove a product

--- myfrontend/src/pages/CartPage.js
+++ myfrontend/src/pages/CartPage.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Alert, Col, Container, Row, Table } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import "./CartPage.css";
+import { useIncreaseCartProductMutation, useDecreaseCartProductMutation, useRemoveFromCartMutation } from "../services/appApi";
 //import CheckoutForm from "../components/CheckoutForm";
-//import { useIncreaseCartProductMutation, useDecreaseCartProductMutation, useRemoveFromCartMutation } from "../services/appApi";
-//import "./CartPage.css";
 
 //const stripePromise = loadStripe("your_stripe_publishable_key");
 
@@ -12,23 +12,63 @@ function CartPage() {
     const products = useSelector((state) => state.products);
     const userCartObj = user.cart;
     let cart = products.filter((product) => userCartObj[product._id] != null);
-    //const [increaseCart] = useIncreaseCartProductMutation();
-    //const [decreaseCart] = useDecreaseCartProductMutation();
-    //const [removeFromCart, { isLoading }] = useRemoveFromCartMutation();
+    const [increaseCart] = useIncreaseCartProductMutation();
+    const [decreaseCart] = useDecreaseCartProductMutation();
+    const [removeFromCart, { isLoading }] = useRemoveFromCartMutation();
 
-    //function handleDecrease(product) {
-       // const quantity = user.cart.count;
-        //if (quantity <= 0) return alert("Can't proceed");
-        //decreaseCart(product);
-    //}
+    function handleDecrease(product) {
+        const quantity = user.cart.count;
+        if (quantity <= 0) return alert("Can't proceed");
+        decreaseCart(product);
+    }
 
     return (
         <Container style={{ minHeight: "95vh" }} className="cart-container">
             <Row>
-                <Col>
-                <h1 className="pt-2 h3">Shopping cart</h1>
-                    {cart.length === 0 && <Alert variant="info">Shopping cart is empty. Add products to your cart</Alert>}
-
+                <Col md={7}>
+                    <h1 className="pt-2 h3">Shopping cart</h1>
+                    {cart.length == 0 ? <Alert variant="info">Shopping cart is empty. Add products to your cart</Alert> : <div>Payment here</div>}
+                </Col>
+                <Col md={5}>
+                    {cart.length > 0 && (
+                        <>
+                            <Table responsive="sm" className="cart-table">
+                                <thead>
+                                    <tr>
+                                        <th>&nbsp;</th>
+                                        <th>Product</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th>Subtotal</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {/* loop througt cart products */}
+                                    {cart.map((item) => (
+                                        <tr>
+                                            <td>&nbsp;</td>
+                                            <td>
+                                                { isLoading && <i className="fa fa-times" style={{ marginRight: 10, cursor: 'pointer' }} onClick={() => removeFromCart({ productId: item._id, price: item.price, userId: user._id })}></i>}
+                                                <img src={item.pictures[0].url} style={{ width: 100, height: 100, objectFit: 'cover' }} />
+                                            </td>
+                                            <td>${item.price}</td>
+                                            <td>
+                                                <span className="quantity-indicator">
+                                                    <i className="fa fa-minus-circle" onClick={() => handleDecrease({ productId: item._id, price: item.price, userId: user._id })}></i>
+                                                    <span>{user.cart[item._id]}</span>
+                                                    <i className="fa fa-plus-circle" onClick={() => increaseCart({ productId: item._id, price: item.price, userId: user._id })}></i>
+                                                </span>
+                                            </td>
+                                            <td>${item.price * user.cart[item._id]}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                            <div>
+                                <h3 className="'h4 pt-4">Total: ${user.cart.total}</h3>
+                            </div>
+                        </>
+                    )}
                 </Col>
                 
             </Row>
